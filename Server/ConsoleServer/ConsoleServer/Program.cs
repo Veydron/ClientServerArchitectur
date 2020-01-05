517d96051783303f4c5efe523c1dd0b4b6cbe3cd
Veydron <59504534+Veydron@users.noreply.github.com>
2020-01-05 12:30:35 +0100

    Renamed Projectfolders
    Renamed Projectfolders

---
+++ Server/ConsoleServer/ConsoleServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleServer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "Test Server";
+            Console.WriteLine("Hello im your server Console !");
+            Console.ReadKey();
+        }
+    }
+}
