b43e009803d5ec9581fd6102c5c7f71b723686a3
Veydron <59504534+Veydron@users.noreply.github.com>
2020-01-27 14:13:57 +0100

    rider git set

--- Server/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ Server/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using Telepathy;
 
 namespace ConsoleApplication2
 {
     internal class Program
     {
+        private static Thread threadConsole;
+        
         public static void Main(string[] args)
         {
+            threadConsole = new Thread(new ThreadStart(ConsoleThread));
+            threadConsole.Start();
+            
             Console.Title = "Test Server";
             Console.WriteLine("Hello im your server Console !qww");
             Console.ReadKey();
@@ -18,6 +24,14 @@ namespace ConsoleApplication2
             Console.WriteLine("Server has started");
             Console.ReadKey();      
         }
+
+        private static void ConsoleThread()
+        {
+            while (true)
+            {
+                
+            }
+        }
+        
     }
-    
 }
